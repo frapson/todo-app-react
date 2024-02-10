99abe853d58bc203f4bcf8e87941d0bed12ad12e
Wojciech Kowalik <u2062902@unimail.hud.ac.uk>
2024-02-10 02:11:19 +0000

    added login functionality

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import './TodoApp.css'
+
 export default function TodoApp() {
     return (
         <div className="TodoApp">
@@ -8,14 +11,39 @@ export default function TodoApp() {
         </div>
     )
 }
-
+    
 
 
 
 function LoginComponent() {
+
+    const [username, setUsername] = useState('frapson')
+    const [password, setPassword] = useState('')
+
+    function handleUsernameChange(event) {
+        setUsername(event.target.value)
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value)
+    }
+    
     return (
         <div className="Login">
-            Login Component
+            <div className="LoginForm">
+                <div>
+                    <label>Username</label>
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange}></input>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange}></input>
+                </div>
+                <div>
+                    <button type="button" name="login">SUBMIT</button>
+                </div>
+            </div>
+            
         </div>
     )
 }
